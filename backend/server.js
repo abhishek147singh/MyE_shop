8eb4684b6d296f6a726976d9e3a6efa99ffda0b5
abhishek147singh <83961045+abhishek147singh@users.noreply.github.com>
2022-11-28 07:42:56 -0800

    Add files via upload
    first commit

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import seedRouter from "./routes/seedRoute.js";
+import productRouter from "./routes/ProductRoutes.js";
+import userRouter from "./routes/UserRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import reviewRouter from "./routes/ReviewRoute.js";
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGOODB_URL).then(() => {
+    console.log("connected to DB");
+}).catch(err => {
+    console.log("Error :", err.message);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true }));
+
+app.use('/api/seed/' , seedRouter );
+app.use('/api/products' , productRouter);
+app.use('/api/users' , userRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/reviews', reviewRouter);
+
+
+
+app.use((err , req , res , next) => {
+    res.status(500).send({message : err.message }); 
+})
+
+/**
+ * Storage image related work
+ */
+
+
+
+/**
+ * Storage image related word end at here
+ */
+
+app.get("/api/keys/paypal" , (req , res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
+
+
+app.listen(5000 , () => {
+    console.log("server is running at http://localhost:5000");
+})
